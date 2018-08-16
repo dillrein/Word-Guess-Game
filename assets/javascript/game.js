969f2ec8924a1b505f91a2eaf525cd26142b235b
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-08-15 22:26:12 -0700

    variables and rng picker

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// Variables
+//--------------------------------------------
+var rbcolors =["red", "orange", "yellow", "green", "blue", "violet"];
+var colorchosen = "";
+var lettersincolor = [];
+var blanks = 0;
+var gameboard = [];
+var wrongletters = [];
+
+var wins = 0;
+var losses = 0;
+var attemptsleft = 0;
+
+//Functions
+//----------------------------------------------
+function start () {
+    //rng to pick a color based off number of rbcolor string
+    colorchosen = rbcolors[Math.floor(Math.random() * rbcolors.length)];
+
+    console.log(colorchosen);
+}
+
+
+//Main Process
+//----------------------------------------------
+start();
